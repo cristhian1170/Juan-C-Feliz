c4f06b4f9209c69abf5f774b8998868b8eb5c089
cristhian1170 <57338425+cristhian1170@users.noreply.github.com>
2019-11-04 00:22:55 -0400

    6 y 8 Trabajos realizados
    completos

---
+++ Ejercicio6.php
@@ -0,0 +1,27 @@
+<?php
+
+function url_completa($forwarded_host = false) {
+    $ssl   = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
+    $proto = strtolower($_SERVER['SERVER_PROTOCOL']);
+    $proto = substr($proto, 0, strpos($proto, '/')) . ($ssl ? 's' : '' );
+    if ($forwarded_host && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+        $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    } else {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $host = $_SERVER['HTTP_HOST'];
+        } else {
+            $port = $_SERVER['SERVER_PORT'];
+            $port = ((!$ssl && $port=='80') || ($ssl && $port=='443' )) ? '' : ':' . $port;
+            $host = $_SERVER['SERVER_NAME'] . $port;
+        }
+    }
+    $request = $_SERVER['REQUEST_URI'];
+    return $proto . '://' . $host . $request;
+}
+?>
+
+
+<div id="url"><?php
+    $url = url_completa();
+    echo htmlspecialchars($url);
+?></div>
